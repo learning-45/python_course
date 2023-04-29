c68b873172fd22985a6021d03778b2094b58b968
Kola Naresh <nareshlerning45@gmail.com>
2023-04-29 10:17:34 +0530

    1700_1

--- Python_Leetcode_programs/1700.py
+++ Python_Leetcode_programs/1700.py
@@ -1,15 +1,11 @@
 class Solution:
     def countStudents(self, students: list[int], sandwiches: list[int]) -> int:
-        while(len(students)!=0):
-            if(sandwiches[0] not in students):
-                return(len(students))
-            if(students[0]==sandwiches[0]):
-                students.pop(0)
-                sandwiches.pop(0)
+        for sandwich in sandwiches:
+            if sandwich in students:
+                students.remove(sandwich)
             else:
-                a=students.pop(0)
-                students.append(a)
-        return(len(students))
+                break
+        return len(students)
 
 def main():
     sol = Solution()
